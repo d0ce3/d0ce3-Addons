51dd72a499257865d5e40cbe1030d8be9c9a5272
d0ce3 <98363993+d0ce3@users.noreply.github.com>
2025-12-07 00:56:05 +0000

    fix

--- megacmd/modules/backup/__init__.py
+++ megacmd/modules/backup/__init__.py
@@ -1,3 +1,9 @@
+from .core import BackupCore
 from .orchestrator import ejecutar_backup, ejecutar_backup_manual, ejecutar_backup_automatico
 
-__all__ = ['ejecutar_backup', 'ejecutar_backup_manual', 'ejecutar_backup_automatico']
+__all__ = [
+    'BackupCore',
+    'ejecutar_backup', 
+    'ejecutar_backup_manual', 
+    'ejecutar_backup_automatico'
+]
